d8c7a23e770d9ab99ee01b74f00216a6fe70e14b
Laura <lauravdheuvel@telfort.nl>
2014-05-19 19:11:35 +0200

    Het laten zien van alle onderhoudsbeurten, servlet en jsp.

---
+++ Themaproject/src/java/com/oncloud6/atd/users/MaintainanceList.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.oncloud6.atd.users;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Laura
+ */
+public class MaintainanceList {
+    public Date datum;
+    public String beschrijving;
+    public String status;
+    public int manuur;
+    public int onderhoudId;
+    public int autoId;
+    public int bedrijfsId;
+}
