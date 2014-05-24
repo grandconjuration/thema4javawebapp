876cdb6e10f35d081cd63574d512f61c004eb746
Jelle Luteijn <jelle.luteijn@merenita.com>
2014-05-24 15:49:43 +0200

    onderhoud kan aangepast worden

--- Themaproject/src/java/com/oncloud6/atd/maintenances/MaintenancesEditServlet.java
+++ Themaproject/src/java/com/oncloud6/atd/maintenances/MaintenancesEditServlet.java
@@ -6,13 +6,24 @@
 
 package com.oncloud6.atd.maintenances;
 
+import com.oncloud6.atd.mysql.MySQLConnection;
+import com.oncloud6.atd.rights.GroupsEditServlet;
+import com.oncloud6.atd.rights.RightsList;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 /**
  *
@@ -32,22 +43,102 @@ public class MaintenancesEditServlet extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet MaintenancesEditServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet MaintenancesEditServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        MySQLConnection DBConnection = new MySQLConnection();
+        try {
+            Connection connect = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connect.prepareStatement("SELECT * FROM atd.onderhoud WHERE onderhoud_id = ?");
+                    
+            String id = request.getParameter("id");
+
+            preparedStatement.setString(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            
+            if (!resultSet.next()) {
+                preparedStatement.close();
+                connect.close();
+                response.sendRedirect("maintenances");
+            }else{
+                request.setAttribute("id", id);
+                request.setAttribute("description", resultSet.getString("onderhoud_beschrijving"));
+                request.setAttribute("manhour", resultSet.getString("onderhoud_manuur"));
+                String status = resultSet.getString("onderhoud_status");
+                preparedStatement.close();
+                
+                ArrayList<DropdownValues> values = new ArrayList<DropdownValues>();
+                DropdownValues value;
+                
+                value = new DropdownValues();
+                value.key = "added";
+                value.value = "toegevoegd";
+                if(status.equals("added")) {
+                    value.selected = true;
+                }else{
+                    value.selected = false;
+                }
+                values.add(value);
+                
+                value = new DropdownValues();
+                value.key = "planned";
+                value.value = "ingeplanned";
+                if(status.equals("planned")) {
+                    value.selected = true;
+                }else{
+                    value.selected = false;
+                }
+                values.add(value);
+                
+                value = new DropdownValues();
+                value.key = "working";
+                value.value = "wordt aan gewerkt";
+                if(status.equals("working")) {
+                    value.selected = true;
+                }else{
+                    value.selected = false;
+                }
+                values.add(value);
+                
+                value = new DropdownValues();
+                value.key = "done";
+                value.value = "afgerond";
+                if(status.equals("done")) {
+                    value.selected = true;
+                }else{
+                    value.selected = false;
+                }
+                values.add(value);
+                
+                request.setAttribute("status", values);
+                
+                /*preparedStatement = connect.prepareStatement("SELECT atd.rechten.rechten_id as id_default, atd.rechten.rechten_key as name, atd.rechten.rechten_type as type, atd.rechten.rechten_value as value_default, atd.rechten_groepen.rechten_id as id_group, atd.rechten_groepen.rechten_groepen_value as value_group FROM atd.rechten LEFT OUTER JOIN atd.rechten_groepen ON atd.rechten.rechten_id = atd.rechten_groepen.rechten_id AND atd.rechten_groepen.groepen_id = ? ORDER BY atd.rechten.rechten_key ASC;");
+                preparedStatement.setString(1, id);
+                resultSet = preparedStatement.executeQuery();
+
+                ArrayList<RightsList> values = new ArrayList<RightsList>();
+
+                while (resultSet.next()) {
+                    RightsList rights = new RightsList();
+                    rights.id = resultSet.getString("id_default");
+                    rights.naam = resultSet.getString("name");
+                    rights.defaultValue = resultSet.getString("value_default");
+                    rights.value = resultSet.getString("value_group");
+
+                    values.add(rights);
+                } 
+                request.setAttribute("rightlist", values);
+                preparedStatement.close();
+                connect.close();*/
+                
+                RequestDispatcher rd = null;
+                HttpSession session = request.getSession(true);
+
+                rd = request.getRequestDispatcher("maintenances/edit.jsp");
+                rd.forward(request, response);
+            }
+
+        } catch (Exception ex) {
+            Logger.getLogger(MaintenancesEditServlet.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -73,17 +164,36 @@ public class MaintenancesEditServlet extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
-    }
+        MySQLConnection DBConnection = new MySQLConnection();
+        try {
+            Connection connect = DBConnection.getConnection();
+            PreparedStatement preparedStatement = connect.prepareStatement("UPDATE atd.onderhoud SET onderhoud_beschrijving = ?, onderhoud_manuur = ?, onderhoud_status = ? WHERE onderhoud_id = ?");
 
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
+            String description = request.getParameter("description");
+            String manhour = request.getParameter("manhour");
+            String status = request.getParameter("status");
+            String id = request.getParameter("id");
+
+            preparedStatement.setString(1, description);
+            preparedStatement.setString(2, manhour);
+            preparedStatement.setString(3, status);
+            preparedStatement.setString(4, id);
+            preparedStatement.executeUpdate();
+
+            request.setAttribute("msg", "De groep  is succesvol toegevoegd!");
+
+            //niet vergeten om alles te sluiten :)
+            preparedStatement.close();
+            connect.close();
+
+            RequestDispatcher rd = null;
+            HttpSession session = request.getSession(true);
+
+            processRequest(request, response);
+
+        } catch (Exception ex) {
+            Logger.getLogger(MaintenancesEditServlet.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
 
 }
--- Themaproject/web/maintenances/edit.jsp
+++ Themaproject/web/maintenances/edit.jsp
@@ -4,6 +4,8 @@
     Author     : Jelle
 --%>
 
+<%@page import="com.oncloud6.atd.maintenances.DropdownValues"%>
+<%@page import="java.util.ArrayList"%>
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
 <jsp:include page="../theme/header.jsp" />
 <div class="jumbotron">
@@ -14,6 +16,45 @@
 
 <div class="container">
     <div class="row">
-        
+        <% Object msg = request.getAttribute("msg");
+        if(msg != null) {
+            out.println("<div class=\"alert alert-success\">" + msg + "</div>");
+        }%>
+        <form action="" method="post">
+        <div class="input-group input-group-lg">
+            <span class="input-group-addon">#</span>
+            <input type="text" class="form-control" placeholder="Beschrijving" name="description" value="<%
+            Object description = request.getAttribute("description");
+            if(description != null) {
+                out.println(description);
+            }
+            %>">
+        </div>
+        <div class="input-group input-group-lg">
+            <span class="input-group-addon">#</span>
+            <input type="text" class="form-control" placeholder="Man uren" name="manhour" value="<%
+            Object manhour = request.getAttribute("manhour");
+            if(manhour != null) {
+                out.println(manhour);
+            }
+            %>">
+        </div>
+        <div class="input-group input-group-lg">
+            <span class="input-group-addon">#</span>
+            <select name="status" class="form-control">
+                <%  
+                // retrieve your list from the request, with casting 
+                ArrayList<DropdownValues> values = (ArrayList<DropdownValues>) request.getAttribute("status");
+                // print the information about every category of the list
+                for(DropdownValues dlist : values) {
+                    %>
+                    <option value="<%=dlist.key %>"<% if(dlist.selected) { out.println(" selected=\"true\"");} %>><%=dlist.value %></option>
+                    <%
+                }
+                %>
+            </select>
+        </div>
+           <br/> <input type="submit"  class="btn btn-default" value="Voltooien" />
+        </form>
     </div>
     <jsp:include page="../theme/footer.jsp" />
