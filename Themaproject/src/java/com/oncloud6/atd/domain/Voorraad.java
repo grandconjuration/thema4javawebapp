55bb5d2e25bb8ac5479fd82588a365f136f0c568
Simon Whiteley <simonwhiteley@hotmail.com>
2014-06-22 12:09:54 +0200

    domain updateje

--- Themaproject/src/java/com/oncloud6/atd/domain/Voorraad.java
+++ Themaproject/src/java/com/oncloud6/atd/domain/Voorraad.java
@@ -6,19 +6,43 @@
 
 package com.oncloud6.atd.domain;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
- * @author Laura
+ * @author Laura, Simon
  */
+@Entity
+@Table(name = "onderdeel")
 public class Voorraad {
-    public String naam;
-    public int hoeveelheid;
-    public Double prijs;
+    @Id
+    @GeneratedValue
+    @Column(name = "onderdeel_id")
+    private int id;
+    
+    @Column(name = "onderdeel_naam")
+    private String naam;
+    
+    @Column(name = "onderdeel_hoeveelheid")
+    private int hoeveelheid;
+    
+    @Column(name = "onderdeel_prijs")
+    private Double prijs;
+    
+    public Voorraad() { }
     
     public Voorraad(String nm, int hh, Double p){
         naam = nm;
         hoeveelheid = hh;
         prijs = p;
+    }
+    
+    public int getId() {
+	   return id;
     }
 
     public String getNaam() {
