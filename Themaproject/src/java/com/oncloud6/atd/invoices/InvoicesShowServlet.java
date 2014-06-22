dabff8bbb613457925b70833cd4a3a27bf77ad54
Jelle Luteijn <jelle.luteijn@merenita.com>
2014-06-23 01:08:50 +0200

    begin invoicesshow en aanpassing aan glassfish
    anders krijgen we problemen met pdfjes

--- Themaproject/src/java/com/oncloud6/atd/invoices/InvoicesShowServlet.java
+++ Themaproject/src/java/com/oncloud6/atd/invoices/InvoicesShowServlet.java
@@ -6,13 +6,18 @@
 
 package com.oncloud6.atd.invoices;
 
+import com.oncloud6.atd.domain.Factuur;
+import com.oncloud6.atd.hibernate.HibernateConnector;
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 /**
  *
@@ -21,33 +26,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet(name = "InvoicesShowServlet", urlPatterns = {"/invoicesshow"})
 public class InvoicesShowServlet extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try (PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet InvoicesShowServlet</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet InvoicesShowServlet at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
-        }
-    }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
     /**
      * Handles the HTTP <code>GET</code> method.
      *
@@ -59,31 +37,33 @@ public class InvoicesShowServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        processRequest(request, response);
+        SessionFactory factory = new HibernateConnector().getSessionFactory();
+        Session hibernateSession = factory.openSession();
+        Transaction tx = null;
+        
+        boolean idSet = false;
+        if (request.getParameter("id") == null || request.getParameter("id").equals("")) {
+            response.sendRedirect("invoices");
+        } else {
+            try {
+                Factuur gekozenFactuur = new Factuur();
+                gekozenFactuur = (Factuur)hibernateSession.get(Factuur.class, Integer.parseInt(request.getParameter("id")));
+                if(gekozenFactuur == null) {
+                    response.sendRedirect("invoices");
+                }else{
+                    
+                }
+            } catch (HibernateException e) {
+                if (tx != null) {
+                    tx.rollback();
+                }
+                e.printStackTrace();
+            } finally {
+                hibernateSession.close();
+            }
+        }
+        
+        
     }
-
-    /**
-     * Handles the HTTP <code>POST</code> method.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        processRequest(request, response);
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
-     */
-    @Override
-    public String getServletInfo() {
-        return "Short description";
-    }// </editor-fold>
 
 }
